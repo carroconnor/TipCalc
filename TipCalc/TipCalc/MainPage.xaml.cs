71e23bf2ea2e905f04ba4ffc3907d4dca1f3cdd0
Carr OConnor <carr.oconnor@gmail.com>
2018-04-24 14:31:19 -0400

    broken

--- TipCalc/TipCalc/MainPage.xaml.cs
+++ TipCalc/TipCalc/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using Xamarin.Forms;
 
 namespace TipCalc
@@ -14,52 +13,12 @@ namespace TipCalc
             BindingContext = vm;
         }
 
-        public void CalculateTip(object sender, EventArgs e)
+        public async void BtnPercentage_OnClicked(object sender, EventArgs e)
         {
-            Regex inputValidation = new Regex(@"^\d+(\.\d{1,2})?$");
-
-            var TestBillDecimalPlace = inputValidation.IsMatch(BillEntry.Text);
-            var TestTipDecimalPlace = inputValidation.IsMatch(TipEntry.Text);
-
-            if (TestBillDecimalPlace && TestTipDecimalPlace == true)
-            {
-                var BillIsNumber = float.TryParse(BillEntry.Text, out _);
-                var TipIsNumber = float.TryParse(TipEntry.Text, out _);
-
-                if (BillIsNumber && TipIsNumber)
-                {
-                    var BillValue = float.Parse(BillEntry.Text);
-                    var TipValue = float.Parse(TipEntry.Text);
-
-                    if (BillValue < 0 || TipValue < 0)
-                    {
-                        finalPercentage.Text = "Please Enter In A Positive Number";
-                    }
-
-                    else
-                    {
-                        var TipPercentage = vm.TipCalculate(BillValue, TipValue);
-
-                        if (!float.IsNaN(TipPercentage))
-                        {
-                            finalPercentage.IsEnabled = true;
-                            finalPercentage.Text = "Your Table Tipped You %" + Math.Round(TipPercentage, 4);
-                        }
-                    }
-                }
-
-                else
-                {
-                    finalPercentage.Text = "Enter in a number";
-                }
-            }
-
-            else
-            {
-                finalPercentage.Text = "That doesn't look like a valid money value.";
-            }
-
-
+            await Navigation.PushAsync(new PercentagePage());
+            btnPercentage.IsEnabled = true;
         }
     }
 }
+
+
---
+++ TipCalc/TipCalc/Model/HourlyWagePageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TipCalc.Model
+{
+    public class HourlyWagePageViewModel
+    {
+    }
+}
