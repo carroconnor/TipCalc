102fa555c3bcab0064dc1fa6396baecf319ff5d2
Carr OConnor <carr.oconnor@gmail.com>
2018-04-26 10:38:54 -0400

    added hourly wage page and function

--- TipCalc/TipCalc/App.xaml.cs
+++ TipCalc/TipCalc/App.xaml.cs
@@ -1,34 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 using Xamarin.Forms;
 
 namespace TipCalc
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-			MainPage = new TipCalc.MainPage();
-		}
+            //MainPage = new TipCalc.MainPage();
+            MainPage = new TabbedPage
+            {
+                Children =
+                {
+                    new TipCalc.MainPage(),
+                    new TipCalc.PercentagePage(),
+                    new TipCalc.HourlyWagePage()
+                }
+            };
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
--- TipCalc/TipCalc/Model/HourlyWagePageViewModel.cs
+++ TipCalc/TipCalc/Model/HourlyWagePageViewModel.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TipCalc.Model
+﻿namespace TipCalc.Model
 {
     public class HourlyWagePageViewModel
     {
+        public float MoneyMadeValue { get; set; }
+        public float HourValue { get; set; }
+        public float HourlyWage { get; set; }
+
+        public float WageCalculate(float MoneyMadeValue, float HourValue)
+        {
+            HourlyWage = (MoneyMadeValue / HourValue);
+
+            return HourlyWage;
+        }
     }
 }
